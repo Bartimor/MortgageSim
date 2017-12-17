fa24b2e6a5db6ca0d8596033b3b6cad9ee4414b7
Bartimor <bartosz.guzdziol@gmail.com>
2017-12-17 15:12:49 +0100

    Initial Layout of first view Income and expanses

--- MortgageSim/MainWindow.xaml.cs
+++ MortgageSim/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,74 @@ namespace MortgageSim
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        ///////////////////////////////////////////////////////////////////// Income and expanses 
+
+        private void textBoxMonthlyIncome_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBoxMonthlyIncome.Text == "" && textBoxMonthlyExpenses.Text == "")
+            {
+                labelMonthlySavings.Content = 0;
+            }
+            else if (textBoxMonthlyExpenses.Text == "")
+            {
+                labelMonthlySavings.Content = textBoxMonthlyIncome.Text;
+            }
+            else if (textBoxMonthlyIncome.Text == "")
+            {
+                int income = 0;
+                int expenses = int.Parse(textBoxMonthlyExpenses.Text);
+                labelMonthlySavings.Content = income - expenses;
+            }
+            else
+            {
+                int income = int.Parse(textBoxMonthlyIncome.Text);
+                int expenses = int.Parse(textBoxMonthlyExpenses.Text); 
+                labelMonthlySavings.Content = income - expenses;
+            }
+        }
+
+        private void textBoxMonthlyExpenses_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBoxMonthlyIncome.Text == "" && textBoxMonthlyExpenses.Text == "")
+            {
+                labelMonthlySavings.Content = 0;
+            }
+            else if (textBoxMonthlyExpenses.Text == "")
+            {
+                labelMonthlySavings.Content = textBoxMonthlyIncome.Text;
+            }
+            else if (textBoxMonthlyIncome.Text == "")
+            {
+                labelMonthlySavings.Content = 0;
+            }
+            else
+            {
+                int income = int.Parse(textBoxMonthlyIncome.Text);
+                int expenses = int.Parse(textBoxMonthlyExpenses.Text);
+                labelMonthlySavings.Content = income - expenses;
+            }
+        }
+
+        private void textBoxMonthlyIncome_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+
+        private void textBoxMonthlyExpenses_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            e.Handled = Regex.IsMatch(e.Text, "[^0-9]+");
+        }
+        ///////////////////////////////////////////////////////////////////// Income and expanses  
+
+
+
+        private void buttonNext_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
